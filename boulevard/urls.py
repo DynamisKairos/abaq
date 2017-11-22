1aef34faa5243fa925e4e5edace6f6eae9498860
Gerardo J. Vasquez R.- Oficina <vasquezgerardojr@gmail.com>
2017-11-22 14:06:34 -0400

    correccion con smart selects

--- boulevard/urls.py
+++ boulevard/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^chaining/', include('smart_selects_urls')),
+    url(r'^chaining/', include('smart_selects.urls')),
 ]
